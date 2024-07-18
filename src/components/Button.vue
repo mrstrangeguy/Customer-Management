7547c7dee527109fecb13cfccb78d47c38f1b91b
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-07-18 19:21:02 +0530

    feat:add icons

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,23 +1,21 @@
 <template>
     <button :class="{'button-default':true,'button-version-1':isCurrentClass(1)}">
-    
+    <i class="icon-default"></i>
     </button>
 </template>
 
 <script setup lang="ts">
-import {computed} from "vue"
-
 const props = defineProps<{
     version:number;
 }>()
 
 //function
 const isCurrentClass = (versionNumber:number) => {
-   return props.version === versionNumber
+   return props.version === versionNumber;
 }
 </script>
 
-<style  scoped>
+<style lang="scss" scoped>
 .button-version-1 {
    padding: 5px 12px 5px 8px;
    box-shadow: 0 1px 3px rgba(0,0,0,.24);
