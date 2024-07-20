9bc81f88eab5ee9562db955928f4ad649c839490
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-07-20 07:43:13 +0530

    save commit

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <div>
+    <div class="dropdown-header">
+    <i class=""></i>
+    </div>
+    </div>    
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  version:Number,
+
+});
+</script>
+
+<style scoped>
+.dropdown-header {
+    padding: 9px 16px 9px 0px;
+    display: flex;
+}
+</style>
