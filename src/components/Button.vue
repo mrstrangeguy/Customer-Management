74e94432b2337affd15318bc3a67affcf6b72b15
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-07-23 14:51:37 +0530

    feat:change props structure

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,106 +1,110 @@
 <template>
-    <button :class="{ 'button-default': true, [buttonType]: true,[variant]:true }">
-        <i :class="{ 'default-icon': true, [icon]: true, [`${getIconType()}`]: true }"></i>
-        <span :class="{ 'text-type': true }">{{ text }}</span>
-    </button>
+  <button
+    :class="{ 'button-default': true, [variant]: true }"
+    :style="{ background: bgColor }"
+  >
+    <i
+      :class="{
+        'default-icon': true,
+        [icon]: true,
+        'enable-right-margin': isMarginRightEnabled(),
+      }"
+    ></i>
+    <span :class="{ 'text-style': true }">{{ text }}</span>
+  </button>
 </template>
 
 <script setup lang="ts">
-import {ButtonTypes} from '../types/buttonTypes'
-
 const props = defineProps({
-    version: String,
-    buttonType: {
-        type: String,
-        default:''
-    },
-    text: {
-        type: String,
-        required: true
-    },
-    icon: {
-        type: String,
-        default: ''
-    },
-    variant: {
-        type:String,
-        default:''
-    }
-})
+  text: {
+    type: String,
+    default: "",
+  },
+  icon: {
+    type: String,
+    default: "",
+  },
+  variant: {
+    type: String,
+    default: "common common--primary",
+  },
+  bgColor: String,
+});
 
 //functions
-const getIconType = () => {
-    if (!props.buttonType) return
-
-    // if (props.variant === ButtonTypes.common.variant.primary) {
-    //     return 'button-icon-type-1'
-    // }
-    // else if(props.variant === ButtonTypes.icon.variant.primary){
-    //     return 'button-icon-type-2'
-    // }
-}
-
+const isMarginRightEnabled = () => {
+  return props.variant === "common common--primary";
+};
 </script>
 
 <style lang="scss" scoped>
 .button-default {
-    cursor: pointer;
-    background-color: transparent;
+  cursor: pointer;
+  background-color: transparent;
 }
 
 .default-icon {
-    font-size: 16px;
+  font-size: 16px;
+  display: block;
+}
+
+.enable-right-margin {
+  margin-right: 4px;
 }
 
 .button-icon-type-1,
 .button-icon-type-2 {
-    color: rgba(0, 0, 0, 0.87);
+  color: rgba(0, 0, 0, 0.87);
 
-    &::before {
-        display: block;
-    }
+  &::before {
+    display: block;
+  }
 }
 
-.common-button {
-    border-radius: 2px;
-    display: flex;
-    align-items: center;
-    text-transform: uppercase;
-    font-weight: 500;
+.button-default.common {
+  border-radius: 2px;
+  display: flex;
+  align-items: center;
+  text-transform: uppercase;
+  font-weight: 500;
 
-    &--primary {
-        padding: 5px 12px 5px 8px;
-        box-shadow: 0 1px 3px rgba(0, 0, 0, .24);
-        background-color: #03a9f4;
-        height: 28px;
-        color: white;
-    }
+  &--primary {
+    padding: 5px 12px 5px 8px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.24);
+    height: 28px;
+    color: white;
+  }
 
-    &--secondary {
-        padding: 5px 12px;
-        border: 1px solid rgba(0, 0, 0, 0.24);
-        color: rgba(0, 0, 0, 0.87);
-        min-width: 90px;
-        justify-content: center;
-        height: 26px;
-    }
+  &--secondary {
+    padding: 5px 12px;
+    border: 1px solid rgba(0, 0, 0, 0.24);
+    color: rgba(0, 0, 0, 0.87);
+    min-width: 90px;
+    justify-content: center;
+    height: 26px;
+  }
+}
+
+.button-default.icon {
+  padding: 5px;
+  border-radius: 50%;
+  transition: 0.4s ease;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.12);
+  }
 }
 
 .button-icon-type-1 {
-    margin-right: 4px;
-    color: white;
+  margin-right: 4px;
+  color: white;
 }
 
-.text-type {
-    text-transform: uppercase;
-    font-size: 13px;
-    font-weight: 500;
-    display: block;
-    letter-spacing: 0.52px;
+.text-style {
+  text-transform: uppercase;
+  font-size: 13px;
+  font-weight: 500;
+  display: block;
+  letter-spacing: 0.52px;
 }
-
-.button-icon-type-2 {
-    font-size: 18px;
-}
-
 </style>
