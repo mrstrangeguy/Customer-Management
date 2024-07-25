439626fd8593861f4e39305f6ca17b9b560aa1a3
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-07-25 19:01:54 +0530

    save commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import Dropdown from './components/Dropdown.vue';
 import { DropdownEnum } from './types/DropdownEnum';
+
+//text data
+const sidebarDropdownData = ref({title:'CRM',items:['Contact List']})
 </script>
 
 <template>
-<Dropdown icon-left="dx-icon dx-icon-user" icon-right="dx-treeview-toggle-item-visibility" :variant="DropdownEnum.Sidebar"/>
+<Dropdown icon-left="dx-icon dx-icon-user" :data="sidebarDropdownData" :variant="DropdownEnum.Sidebar"/>
 <!-- <Dropdown :variant="DropdownEnum.Tool"/> -->
 <!-- <Dropdown :variant="DropdownEnum.Profile"/> -->
 </template>
