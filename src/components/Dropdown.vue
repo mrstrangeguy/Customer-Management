0b520964e6c5b9de271016c6f82162ea8b8ecf28
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-07-29 13:44:18 +0530

    feat:create dropdown component

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,81 +1,104 @@
 <template>
-    <div :class="{'cursor-pointer':true,[containerStyle]:true}" style="width: 250px;">
-    <div :class="{'dropdown-header':true,[headerStyle]:true}" @click="toggleContentVisibility">
-    <div class="dropdown-header__content">
-    <i :class="{'default-icon':true,[mainIcon]:true}"/>
-    <span :class="{[textStyle]:true}">{{ text }}</span>
+  <div
+    :class="{ 'cursor-pointer z-100 relative': true, [containerStyle]: true }"
+  >
+    <div
+      :class="{ 'dropdown-header z-100 relative': true, [headerStyle]: true }"
+      @click.stop="toggleContentVisibility"
+    >
+      <div class="dropdown-header__content">
+        <i :class="{ 'default-icon': true, [mainIcon]: true }" />
+        <span :class="{ [textStyle]: true }">{{ text }}</span>
+      </div>
+      <i
+        :class="{
+          'default-icon dx-treeview-toggle-item-visibility': true,
+          [arrowIcon]: true,
+          'dx-treeview-toggle-item-visibility-opened': isHeaderClicked,
+        }"
+      />
     </div>
-     <i :class="{'default-icon dx-treeview-toggle-item-visibility':true,[arrowIcon]:true,'dx-treeview-toggle-item-visibility-opened':isHeaderClicked}"/>
-    </div>
-    <div :class="{'dropdown__content transition-all duration-[700ms] ease-linear':true,'max-h-fit':isHeaderClicked,'max-h-0':!isHeaderClicked}">
+    <div
+      :class="{
+        'dropdown__content z-[-1] relative overflow-hidden transition-all': true,
+        'max-h-[1000px] duration-[1200ms] ease-in': isHeaderClicked,
+        'max-h-0 duration-[1500ms] ease-out': !isHeaderClicked,
+      }"
+    >
       <slot name="dropdown-items"></slot>
     </div>
-    </div>
-   <!--'max-h-fit':isHeaderClicked,'max-h-0':!isHeaderClicked-->
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 
 const isHeaderClicked = ref(false);
 
 const toggleContentVisibility = () => {
   isHeaderClicked.value = !isHeaderClicked.value;
-}
- 
-const props = defineProps({
+};
+
+defineProps({
   headerStyle: {
-   type:String,
-   required:true
+    type: String,
+    required: true,
   },
   mainIcon: {
-    type:String,
-    default:''
+    type: String,
+    default: "",
   },
   text: {
-    type:String,
-    default:''
+    type: String,
+    default: "",
   },
   textStyle: {
-    type:String,
-    default:''
+    type: String,
+    default: "",
   },
   arrowIcon: {
-    type:String,
-    default:''
+    type: String,
+    default: "",
   },
   containerStyle: {
-    type:String,
-    default:''
-  }
-})
+    type: String,
+    default: "",
+  },
+});
 </script>
 
 <style lang="scss" scoped>
-
-.dropdown {
-  &__content {
-    -webkit-transition: max-height 1s;
-  -moz-transition: max-height 1s;
-  -ms-transition: max-height 1s;
-  -o-transition: max-height 1s;
-  transition: max-height 1s;
-  } 
+.red-box {
+  position: absolute;
+  top: 5px;
+  background-color: red;
+  left: 10px;
+  width: 20px;
+  height: 20px;
 }
-
 .dropdown-header {
   display: flex;
   align-items: center;
-  gap:12px;
+  gap: 12px;
 
   &__content {
     display: flex;
     align-items: center;
     flex-grow: 1;
-   
   }
-
 }
-
-
 </style>
+
+<!-- <Dropdown header-style="py-[9px] pr-[16px] bg-[#0000000a]"
+ main-icon="dx-icon dx-icon-user block text-black w-12 h-[18px] leading-[18px] text-[18px] text-center"
+text-style="text-[13px] font-bold leading-[15px]" text="CRM" arrow-icon="text-[18px] block h-[18px] leading-[18px]">
+
+<template v-slot:dropdown-items>
+<div class="py-[9px] pr-[16px] flex items-center bg-[#0000000a]">
+<span class="block pl-12 text-[13px] leading-[16.7141px]">Contact List</span>
+</div>
+<div class="py-[9px] pr-[16px] flex items-center bg-[#0000000a]">
+<span class="block pl-12 text-[13px] leading-[16.7141px]">Contact List</span>
+</div>
+</template>
+</Dropdown> -->
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-     transitionProperty: {
-      'height':'height',
-      'opacity':'opacity'
-     },
-     transitionDuration: {
-      'height':'700ms',
-      'opacity':'400ms'
-     }
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
