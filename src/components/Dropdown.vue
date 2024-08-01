d14d17baf7e69efcdf76060e2d0d4acb865e6b4b
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-01 17:44:02 +0530

    refactor:change sytax

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -24,7 +24,7 @@
         'max-h-0 duration-[1500ms] ease-out': !isHeaderClicked,
       }"
     >
-      <slot name="dropdown-items"></slot>
+      <slot name="dropdown-items" />
     </div>
   </div>
 </template>
@@ -90,19 +90,19 @@ defineProps({
 </style>
 
 <!-- <Dropdown
-header-style="py-[9px] pr-[16px] bg-[#0000000a]"
+header-style="py-[9px] pr-4 bg-[#0000000a]"
 main-icon="dx-icon dx-icon-user block text-black w-12 h-[18px] leading-[18px] text-[18px] text-center"
 text-style="text-[13px] font-bold leading-[15px]"
 text="CRM"
 arrow-icon="text-[18px] block h-[18px] leading-[18px]"
 >
 <template v-slot:dropdown-items>
-  <div class="py-[9px] pr-[16px] flex items-center bg-[#0000000a]">
+  <div class="py-[9px] pr-4 flex items-center bg-[#0000000a]">
     <span class="block pl-12 text-[13px] leading-[16.7141px]"
       >Contact List</span
     >
   </div>
-  <div class="py-[9px] pr-[16px] flex items-center bg-[#0000000a]">
+  <div class="py-[9px] pr-4 flex items-center bg-[#0000000a]">
     <span class="block pl-12 text-[13px] leading-[16.7141px]"
       >Contact List</span
     >
@@ -111,14 +111,14 @@ arrow-icon="text-[18px] block h-[18px] leading-[18px]"
 </Dropdown>
 
 <Dropdown
-header-style="py-[9px] pr-[16px] bg-[#0000000a]"
+header-style="py-[9px] pr-4 bg-[#0000000a]"
 main-icon="dx-icon dx-icon-user block text-black w-12 h-[18px] leading-[18px] text-[18px] text-center"
 text-style="text-[13px] font-bold leading-[15px]"
 text="CRM"
 arrow-icon="text-[18px] block h-[18px] leading-[18px]"
 >
 <template v-slot:dropdown-items>
-  <div class="py-[9px] pr-[16px] flex items-center bg-[#0000000a]">
+  <div class="py-[9px] pr-4 flex items-center bg-[#0000000a]">
     <span class="block pl-12 text-[13px] leading-[16.7141px]"
       >Contact List</span
     >
@@ -128,13 +128,13 @@ arrow-icon="text-[18px] block h-[18px] leading-[18px]"
 
 <Dropdown
 container-style="shadow-[0_2px_4px_0_rgba(0,0,0,.2)]"
-header-style="py-[6px] pl-[11px] pr-[10px] text-[13px] min-h-[40px]"
-arrow-icon="text-[16px] text-[rgba(0, 0, 0, 0.87)] font-normal leading-[16px]"
+header-style="py-1.5 pl-[11px] pr-2.5 text-[13px] min-h-10"
+arrow-icon="text-base text-[rgba(0, 0, 0, 0.87)] font-normal leading-4"
 text="Opportunities"
 >
 <template v-slot:dropdown-items>
   <div class="p-[11px]">
-    <div class="pb-[10px]">
+    <div class="pb-2.5">
       <span class="block text-[13px] leading-[16.7141px]"
         >Incoming Request for Conference Room</span
       >
