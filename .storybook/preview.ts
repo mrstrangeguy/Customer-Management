a33af26beb2977a976570b8044032d441d4ae91f
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-01 14:46:46 +0530

    refactor:remove default stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,5 @@
 import type { Preview } from "@storybook/vue3";
 
-const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
-};
+const preview: Preview = {};
 
 export default preview;
