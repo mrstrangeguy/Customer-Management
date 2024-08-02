807bf33ad1d4647ee8a0b8df88dd6f8ccaa8d95f
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-02 16:51:49 +0530

    refactor:add profile variant to options-dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
-<script setup lang="ts">
-import OptionsDropdown from './components/OptionsDropdown.vue'
-</script>
+<script setup lang="ts"></script>
 
-<template>
-   <options-dropdown text="ALL" arrow-icon="dx-icon dx-icon-spindown dx-icon-right"/>
-   <options-dropdown profile-url="https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/employees/01.png"/>
-</template>
+<template></template>
 
 <style lang="scss" scoped></style>
