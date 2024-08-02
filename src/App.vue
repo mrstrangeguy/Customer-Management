02783ef2b63e205468a7f87c13c8714947cd4947
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-02 20:55:08 +0530

    feat:create text component for profile card

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Text from "./components/Text.vue";
+</script>
 
-<template></template>
+<template>
+  <Text title="Position" description="ACME" description-color="#03a9f4" />
+  <Text icon="dx-icon dx-icon-tel" description="sample5@gmail.com" />
+</template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/Text.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    style="margin-left: 20px; margin-top: 20px"
+    :class="{ 'h-8': true, 'flex items-center': icon }"
+  >
+    <div v-if="icon" class="p-[6px] flex items-center">
+      <i
+        :class="`default-icon block ${icon} h-4 w-4 text-center leading-4 text-[#0000001a]`"
+      />
+    </div>
+    <span
+      v-if="!icon"
+      class="block text-[10px] leading-[10px] text-[#00000099]"
+      >{{ title }}</span
+    >
+    <div
+      :class="{
+        [`text-xs leading-[14px] mt-1 text-[${descriptionColor}]`]: true,
+        'mt-0': icon,
+        'py-[9px] leading=[15.4284px] px-3 text-black': icon,
+      }"
+    >
+      {{ description }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  icon: String,
+  title: String,
+  description: String,
+  descriptionColor: {
+    type: String,
+    default: "#000",
+  },
+});
+</script>
+
+<style scoped>
+.hello {
+  color: #03a9f4;
+  color: #0000001a;
+}
+</style>
