923918fab1f735c0dfaff1a808e4a3847077ed85
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-02 14:46:49 +0530

    refactor:change class syntax

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import OptionsDropdown from './components/OptionsDropdown.vue'
 
 <template>
    <options-dropdown text="ALL" arrow-icon="dx-icon dx-icon-spindown dx-icon-right"/>
+   <options-dropdown profile-url="https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/employees/01.png"/>
 </template>
 
 <style lang="scss" scoped></style>
