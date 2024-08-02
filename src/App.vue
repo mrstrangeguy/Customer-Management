0f4fa3ce0acf013fc48bb6c27372c171446ea4f4
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-02 20:55:49 +0530

    feat:create text component for profile card

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
-<script setup lang="ts">
-import Text from "./components/Text.vue";
-</script>
+<script setup lang="ts"></script>
 
-<template>
-  <Text title="Position" description="ACME" description-color="#03a9f4" />
-  <Text icon="dx-icon dx-icon-tel" description="sample5@gmail.com" />
-</template>
+<template></template>
 
 <style lang="scss" scoped></style>
