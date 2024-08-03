89b0d5eea843833ca778ffe975f62cc49adeaa98
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-03 18:28:48 +0530

    save commit

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,3 +1,25 @@
-import type { Meta, StoryObj } from '@storybook/vue3';
+
+import type { Meta,StoryObj } from '@storybook/vue3'
 
 import Button from '../components/Button.vue';
+const meta: Meta<typeof Button> = {
+    component:Button
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>
+
+export const Primary: Story = {
+    render: (args:any) => ({
+      components: { Button },
+      setup() {
+        return { args };
+      },
+      template: '<Button v-bind="args" />',
+    }),
+    args: {
+      text:"Add Contact",
+      icon:"dx-icon dx-icon-plus",
+      bgColor:"#03a9f4"
+    },
+  };
