78068abd19bb4880fdce1a329d22f6d789121a3c
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-05 13:45:35 +0530

    reafactor:configure sotrybook settings

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
 <template></template>
 =======
 <template>
-<Button text="Add Contact" icon="dx-icon dx-icon-plus" bgColor="#03a9f4" />
-<Button icon="dx-icon dx-icon-plus" variant="Icon" />
+<Button text="Add Contact" icon="dx-icon dx-icon-moon" bgColor="#03a9f4" />
+<Button icon="dx-icon dx-icon-moon" variant="Icon" />
 </template>
 >>>>>>> 498b1a9 (save commit)
 
