42fe5e2ccccc3955f9eb1fd60068c83fd4d8f8df
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-05 10:54:43 +0530

    save commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts"></script>
 
+<<<<<<< HEAD
 <template></template>
+=======
+<template>
+<Button text="Add Contact" icon="dx-icon dx-icon-plus" bgColor="#03a9f4" />
+<Button icon="dx-icon dx-icon-plus" variant="Icon" />
+</template>
+>>>>>>> 498b1a9 (save commit)
 
 <style lang="scss" scoped></style>
