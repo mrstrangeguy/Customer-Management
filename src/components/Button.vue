0de9523085f0c2659bbfad9d93d18cb390c74d6b
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-05 13:45:35 +0530

    reafactor:configure sotrybook settings

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -3,9 +3,12 @@
     :class="`button-default ${currentButtonClass()}`"
     :style="{ backgroundColor: bgColor }"
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     @mouseover=""
 >>>>>>> ba623bf (save commit)
+=======
+>>>>>>> 534c82b (reafactor:configure sotrybook settings)
   >
     <i :class="`default-icon ${icon}`" />
     <span class="text-style">{{ text }}</span>
