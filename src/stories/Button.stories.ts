d8b854e3f480f87167a32f0a51bf108e282ed3cb
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-06 16:25:24 +0530

    save commit

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,25 +1,59 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import Button from "../components/Button.vue";
 
-import type { Meta,StoryObj } from '@storybook/vue3'
+interface buttonArgs {
+  text?: string;
+  icon?: string;
+  variant?: string;
+  bgColor?: string;
+}
 
-import Button from '../components/Button.vue';
 const meta: Meta<typeof Button> = {
-    component:Button
+  component: Button,
 };
 
 export default meta;
-type Story = StoryObj<typeof Button>
+type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
-    render: (args:any) => ({
-      components: { Button },
-      setup() {
-        return { args };
-      },
-      template: '<Button v-bind="args" />',
-    }),
-    args: {
-      text:"Add Contact",
-      icon:"dx-icon dx-icon-plus",
-      bgColor:"#03a9f4"
+  render: (args: buttonArgs) => ({
+    components: { Button },
+    setup() {
+      return { args };
     },
-  };
+    template: '<Button v-bind="args" />',
+  }),
+  args: {
+    text: "Add Contact",
+    icon: "dx-icon dx-icon-plus",
+    bgColor: "#03a9f4",
+  },
+};
+
+export const Secondary: Story = {
+  render: (args: buttonArgs) => ({
+    components: { Button },
+    setup() {
+      return { args };
+    },
+    template: '<Button v-bind="args"/>',
+  }),
+  args: {
+    text: "details",
+    variant: "Secondary",
+  },
+};
+
+export const Icon: Story = {
+  render: (args: buttonArgs) => ({
+    components: { Button },
+    setup() {
+      return { args };
+    },
+    template: '<Button v-bind="args"/>',
+  }),
+  args: {
+    icon: "dx-icon dx-icon-moon",
+    variant: "Icon",
+  },
+};
