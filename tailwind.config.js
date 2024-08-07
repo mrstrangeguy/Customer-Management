47876eea04fe921d517e8b4a1a29b12aae64e552
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-07 18:56:02 +0530

    save commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      lineHeight: {
+        'leading-0':'0'
+      }
+    },
   },
   plugins: [],
 }
