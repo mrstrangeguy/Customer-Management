b615daec1c775bdad9c544fc71e961d693b45726
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-07 17:36:01 +0530

    save commit

--- src/components/Text.vue
+++ src/components/Text.vue
@@ -1,21 +1,21 @@
 <template>
   <div
-    style="margin-left: 20px; margin-top: 20px"
     :class="{ 'h-8': true, 'flex items-center': icon }"
   >
-    <div v-if="icon" class="p-[6px] flex items-center">
+    <div v-if="icon" class="p-1.5 flex items-center">
       <i
         :class="`default-icon block ${icon} h-4 w-4 text-center leading-4 text-[#0000001a]`"
       />
     </div>
     <span
       v-if="!icon"
-      class="block text-[10px] leading-[10px] text-[#00000099]"
+      class="block text-[10px] leading-2.5 text-[#00000099]"
       >{{ title }}</span
     >
     <div
+    style="color: descriptionColor;"
       :class="{
-        [`text-xs leading-[14px] mt-1 text-[${descriptionColor}]`]: true,
+        [`text-xs leading-3.5 mt-1`]: true,
         'mt-0': icon,
         'py-[9px] leading=[15.4284px] px-3 text-black': icon,
       }"
