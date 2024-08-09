230f1588a8fcf5e56af0ae691b23d21a2b5da419
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-09 14:17:47 +0530

    refactor:Create Test for search component

--- src/tests/Search.spec.ts
+++ src/tests/Search.spec.ts
@@ -1,15 +1,20 @@
-import {it,describe,expect} from "vitest"
-import {shallowMount} from "@vue/test-utils"
-import Search from "../components/Search.vue"
+import { it, describe, expect } from "vitest";
+import { shallowMount } from "@vue/test-utils";
+import Search from "../components/Search.vue";
 
-describe('Search',() => {
-    const SearchElement = shallowMount(Search,{
-        props:{
-            placeholder:"Search Text"
-        }
-    })
+describe("Search", () => {
+  const SearchElement = shallowMount(Search, {
+    props: {
+      placeholder: "Search Text",
+    },
+  });
 
-    it('check placeholder of search component',() => {
-        expect(SearchElement.find('input').element.placeholder).toBe('Search Text')
-    })
-}) 
+  it("check placeholder of search component", () => {
+    expect(SearchElement.find("input").element.placeholder).toBe("Search Text");
+  });
+
+  it("check snapshot of search component", () => {
+    const currentSearch = SearchElement.element;
+    expect(currentSearch).toMatchSnapshot();
+  });
+});
