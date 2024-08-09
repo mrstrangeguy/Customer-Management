940ae503f0dac9bc593f82c51dbcb4e1ae86bdd4
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-09 16:32:32 +0530

    feat:create search test

--- src/tests/Search.spec.ts
+++ src/tests/Search.spec.ts
@@ -1,20 +1,28 @@
-import { it, describe, expect } from "vitest";
-import { shallowMount } from "@vue/test-utils";
+import { it, describe, expect, test } from "vitest";
+import { mount, shallowMount } from "@vue/test-utils";
 import Search from "../components/Search.vue";
 
 describe("Search", () => {
-  const SearchElement = shallowMount(Search, {
+  const searchElement = shallowMount(Search, {
     props: {
       placeholder: "Search Text",
     },
   });
 
   it("check placeholder of search component", () => {
-    expect(SearchElement.find("input").element.placeholder).toBe("Search Text");
+    expect(searchElement.find("input").element.placeholder).toBe("Search Text");
   });
 
   it("check snapshot of search component", () => {
-    const currentSearch = SearchElement.element;
+    const currentSearch = searchElement.element;
     expect(currentSearch).toMatchSnapshot();
+  });
+
+  it("check if input emits a function when input event is triggered", async () => {
+    const wrapper = mount(searchElement);
+    wrapper.vm.$emit("emitInputValue", "This is a value");
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted().emitInputValue[0]).toEqual(["This is a value"]);
   });
 });
