311559b7f45be762f58cc3787dac0340151a9675
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-10 14:29:17 +0530

    save commit

---
+++ src/stories/OptionsDropdown.spec.ts
@@ -0,0 +1,16 @@
+import type {Meta,StoryObj} from "@storybook/vue3"
+import OptionsDropdown from "../components/OptionsDropdown.vue"
+
+const meta: Meta<typeof OptionsDropdown> = {
+    component: OptionsDropdown,
+};
+  
+export default meta;
+type Story = StoryObj<typeof OptionsDropdown>;
+
+export const Primary:Story = {
+    args: {
+    text:"ALL",
+    arrowIcon:"dx-icon dx-icon-spindown dx-icon-right"
+    }
+}
