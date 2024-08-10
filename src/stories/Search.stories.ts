db76d9e48e36567fe8fe5dd766dc47c51c43e066
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-10 11:52:21 +0530

    save commit

---
+++ src/stories/Search.stories.ts
@@ -0,0 +1,10 @@
+import type {Meta,StoryObj} from "@storybook/vue3"
+import Search from "../components/Search.vue"
+
+const meta:Meta<typeof Search> = {
+   component:Search
+}
+
+export default meta
+type Story = StoryObj<typeof Search>
+
