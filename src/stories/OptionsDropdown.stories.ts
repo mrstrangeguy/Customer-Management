7be39abc2a1ebbb83cd61fca6d0cfec5674d66f2
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-10 14:41:37 +0530

    save commit

--- src/stories/OptionsDropdown.stories.ts
+++ src/stories/OptionsDropdown.stories.ts
@@ -1,5 +1,5 @@
 import type {Meta,StoryObj} from "@storybook/vue3"
-import OptionsDropdown from "../components/OptionsDropdown.vue"
+import OptionsDropdown from "../components/OptionsDropdown"
 
 const meta: Meta<typeof OptionsDropdown> = {
     component: OptionsDropdown,
