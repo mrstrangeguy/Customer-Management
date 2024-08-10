a035ba4e5bda2e497ee6d70d02b17d845a570d04
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-10 12:37:01 +0530

    save commit

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script setup lang="ts">
-import {SearchProps} from "../types/SearchProps"
+import { SearchProps } from "../types/SearchProps";
 
 //props
 defineProps<SearchProps>();
--- src/stories/Search.stories.ts
+++ src/stories/Search.stories.ts
@@ -1,13 +1,15 @@
-import type {Meta,StoryObj} from "@storybook/vue3"
-import Search from "../components/Search.vue"
+import type { Meta, StoryObj } from "@storybook/vue3";
+import Search from "../components/Search.vue";
 
-const meta:Meta<typeof Search> = {
-   component:Search
-}
+const meta: Meta<typeof Search> = {
+  component: Search,
+};
 
-export default meta
-type Story = StoryObj<typeof Search>
+export default meta;
+type Story = StoryObj<typeof Search>;
 
-export const Primary:Story = {
-    
-}
+export const Primary: Story = {
+  args: {
+    placeholder: "Search",
+  },
+};
