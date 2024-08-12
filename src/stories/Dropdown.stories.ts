44ca3681443635660db370ea50d6e2248bae13dc
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-12 12:20:41 +0530

    refactor:create slot for stories in dropdown component

--- src/stories/Dropdown.stories.ts
+++ src/stories/Dropdown.stories.ts
@@ -17,6 +17,28 @@ export const Primary: Story = {
     text: "CRM",
     arrowIcon: "text-[18px] block h-[18px] leading-[18px]",
   },
+  render: (args) => ({
+    components: { Dropdown },
+    setup() {
+      return { args };
+    },
+    template: `
+  <Dropdown v-bind="args">
+<template v-slot:dropdown-items>
+  <div class="py-[9px] pr-4 flex items-center bg-[#0000000a]">
+    <span class="block pl-12 text-[13px] leading-[16.7141px]"
+      >Contact List</span
+    >
+  </div>
+  <div class="py-[9px] pr-4 flex items-center bg-[#0000000a]">
+    <span class="block pl-12 text-[13px] leading-[16.7141px]"
+      >Contact List</span
+    >
+  </div>
+</template>
+</Dropdown>
+  `,
+  }),
 };
 
 export const Secondary: Story = {
@@ -26,4 +48,32 @@ export const Secondary: Story = {
     arrowIcon: "text-base text-[rgba(0, 0, 0, 0.87)] font-normal leading-4",
     text: "Opportunities",
   },
+  render: (args) => ({
+    components: { Dropdown },
+    setup() {
+      return { args };
+    },
+    template: `
+<Dropdown
+v-bind="args"
+>
+<template v-slot:dropdown-items>
+  <div class="p-[11px]">
+    <div class="pb-2.5">
+      <span class="block text-[13px] leading-[16.7141px]"
+        >Incoming Request for Conference Room</span
+      >
+      <span class="block text-[13px] leading-[16.7141px]">$2,115</span>
+    </div>
+    <div class="pb-[10px]">
+      <span class="block text-[13px] leading-[16.7141px]"
+        >Incoming Request for Conference Room</span
+      >
+      <span class="block text-[13px] leading-[16.7141px]">$2,115</span>
+    </div>
+  </div>
+</template>
+</Dropdown>
+    `,
+  }),
 };
