78da616978904e33320e3972a7669b7fd31a5667
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-16 08:57:34 +0530

    save commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Customertable from './components/Customertable.vue';
+import uiData from './data/uiData.json'
+import usersData from './data/usersData.json'
 
-<template></template>
+const {userAttributes} = uiData.usersPageData
+const {users} = usersData.userDetails
+
+</script>
+
+<template>
+<Customertable :user-attributes="userAttributes" :users-details="users"/>
+</template>
 
 <style lang="scss" scoped></style>
