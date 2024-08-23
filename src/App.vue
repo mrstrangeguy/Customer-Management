ccce21ca27d01954a30d151a276b587c8a383de4
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-23 15:52:55 +0530

    change file names to pascal case

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
-<script setup lang="ts">
-import Customertable from "./components/Customertable.vue";
-import uiData from "./data/uiData.json";
+<script setup lang="ts"></script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
+
+<!-- import uiData from "./data/uiData.json";
 import usersData from "./data/usersData.json";
 
 const { userAttributes } = uiData.usersPageData;
-const { users } = usersData.userDetails;
-</script>
-
-<template>
-  <Customertable :user-attributes="userAttributes" :users-details="users" />
-</template>
-
-<style lang="scss" scoped></style>
+const { users } = usersData.userDetails; -->
+<!-- <customer-table :user-attributes="userAttributes" :users-details="users" /> -->
