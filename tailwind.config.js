4eadbe4289b6465b9214b4820d48a1a4799e286d
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-30 08:50:00 +0530

    refactor:remove unwanted comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ export default {
         'default-color':'#0000008a',
         'checkbox-after-selected':'#03a6ef1a',
         'checkbox-selected-bg':'#03a9f4',
-        'table-hover-primary':'#f2f2f2', //table-hover-primary
+        'table-hover-primary':'#f2f2f2',
         'td-bg':'#fafafa',
-        'table-border':'#e0e0e0',//table-border
+        'table-border':'#e0e0e0',
         'label':'#00000099',
         'checked':'#03a9f40a',
         'selected':'#cdeefd',
-        'table-hover-secondary':'#00000061',//table-hover-secondary
-        'table-hover-tertiary':'#000000de',//table-hover-tertiary
+        'table-hover-secondary':'#00000061',
+        'table-hover-tertiary':'#000000de',
         'commission':'#03a9f4',
         'terminated':'#de8e8c',
         'salaried':'#2eb52c'
@@ -36,11 +36,11 @@ export default {
         '9.5':'38px',
       },
       screens: {
-        'large':'850px',//large
-        'medium':'750px',//medium
-        'small':'620px',//small
-        'extra-small':'448px',//extra-small
-        'extra-large':'1372px'//extra-large
+        'large':'850px',
+        'medium':'750px',
+        'small':'620px',
+        'extra-small':'448px',
+        'extra-large':'1372px'
       },
       maxWidth: {
         '17.5':'70px',
