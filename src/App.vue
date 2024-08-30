013c15a0f5d7920836832838beba0b07ab4285d5
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-30 08:13:09 +0530

    refactor:remove code from App file

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,5 @@
-<script setup lang="ts">
-import uiData from "./data/uiData.json";
-import usersData from "./data/usersData.json";
-import CustomerTable from "./components/CustomerTable.vue";
+<script setup lang="ts"></script>
 
-const { userAttributes } = uiData.usersPageData;
-const { users } = usersData.userDetails; 
-</script>
-
-<template>
-
-
-<customer-table :user-attributes="userAttributes" :users-details="users" />
-</template>
+<template></template>
 
 <style lang="scss" scoped></style>
-
-<!-- import uiData from "./data/uiData.json";
-import usersData from "./data/usersData.json";
-
-const { userAttributes } = uiData.usersPageData;
-const { users } = usersData.userDetails; -->
-<!-- <customer-table :user-attributes="userAttributes" :users-details="users" /> -->
