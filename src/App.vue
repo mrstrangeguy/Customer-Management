68f131ee0a91ccaace2c08286322a93940ddad07
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-08-31 15:28:21 +0530

    refactor:removed unwanted code from app file

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,5 @@
-<script setup lang="ts">
-import Button from './components/Button.vue';
-import OptionsDropdown from './components/OptionsDropdown.vue';
-</script>
+<script setup lang="ts"></script>
 
-<template>
-<Button text="Add Contact" icon="dx-icon dx-icon-refresh" bgColor="#03a9f4" />
-<OptionsDropdown profileURL="https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/employees/01.png" />
-<options-dropdown
-    text="ALL"
-    arrow-icon="dx-icon dx-icon-spindown dx-icon-right"
-  >
-    <template v-slot:dropdown-items>
-      <div class="w-[94.97px] bg-[rgba(0,0,0,.12)] mt-1">
-        <span
-          class="block text-[13px] leading-[16px] px-[11px] pt-[10px] pb-[9px]"
-          >All</span
-        >
-      </div>
-      <div class="w-[94.97px] mt-1">
-        <span
-          class="block text-[13px] leading-[16px] px-[11px] pt-[10px] pb-[9px]"
-          >All</span
-        >
-      </div>
-      <div class="w-[94.97px] mt-1">
-        <span
-          class="block text-[13px] leading-[16px] px-[11px] pt-[10px] pb-[9px]"
-          >All</span
-        >
-      </div>
-      <div class="w-[94.97px] mt-1">
-        <span
-          class="block text-[13px] leading-[16px] px-[11px] pt-[10px] pb-[9px]"
-          >All</span
-        >
-      </div>
-    </template>
-  </options-dropdown>
-</template>
+<template></template>
 
 <style lang="scss" scoped></style>
