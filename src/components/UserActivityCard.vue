6e8967bbb72d7f0ec9b9f5e8257685745233b20d
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-09-05 16:09:41 +0530

    feat: add activity card

---
+++ src/components/UserActivityCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="py-2 pl-4 pr-0.5 w-full flex items-center shadow rounded-sm relative border-l-2 border-blue-400"
+  >
+    <span class="block w-full text-3.25">Qualified</span>
+    <div class="px-2.5">
+      <span class="block text-xs text-slate-500">06/10/2020</span>
+      <span class="block text-xs text-nowrap mt-1.25 text-slate-500">
+        Samantha Bright
+      </span>
+    </div>
+    <div>
+      <div class="px-2 py-1.5">
+        <i class="default-icon dx-icon dx-icon-overflow" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
