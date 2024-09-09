16c6dc8596540cc89a0ac910d77635e5dbca9b03
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-09-09 14:15:19 +0530

    refactor: change hardcoded color values in css

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -37,7 +37,7 @@ const props = withDefaults(defineProps<ButtonsProps>(), {
   bgColor: "",
   hoverBg: "#028bc9",
   isRounded: false,
-  buttonStyle: "",
+  buttonStyle: "!px-1.5",
 });
 
 //refs
@@ -86,17 +86,11 @@ const setBackgroundColor = (color: string) => {
 }
 
 .button-default.common-primary {
-  padding: 5px 12px 5px 8px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.24);
   height: 28px;
   color: white;
 
-  &:hover {
-    background-color: rgb(2, 139, 201) !important;
-  }
-
   .default-icon {
-    color: white;
     height: 16px;
     width: 16px;
     line-height: 16px;
@@ -108,7 +102,6 @@ const setBackgroundColor = (color: string) => {
 }
 
 .button-default.common-secondary {
-  padding: 5px 12px;
   border: 1px solid rgba(0, 0, 0, 0.24);
   color: rgba(0, 0, 0, 0.87);
   justify-content: center;
