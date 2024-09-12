b88aedaa07630c43ce02a18f1730ea424ab2e87e
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-09-12 17:00:35 +0530

    refactor: update search storybook

--- src/stories/SearchInput.stories.ts
+++ src/stories/SearchInput.stories.ts
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/vue3";
+
 import SearchInput from "../components/SearchInput.vue";
+import { SearchTypes } from "../Constants";
 
 const meta: Meta<typeof SearchInput> = {
   component: SearchInput,
@@ -9,6 +11,12 @@ export default meta;
 type Story = StoryObj<typeof SearchInput>;
 
 export const Primary: Story = {
+  argTypes: {
+    type: {
+      options: Object.values(SearchTypes),
+      control: "select",
+    },
+  },
   args: {
     placeholder: "Search",
   },
