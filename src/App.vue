6b5dabe80b2e4d972b2ba22247e3acf9bcb01fb7
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-09-26 09:51:30 +0530

    refactor: change order of template,script,style

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
-<script setup lang="ts">
-import CustomerTable from './components/CustomerTable.vue';
-import uiData from './data/uiData.json'
-import usersData from './data/usersData.json'
-</script>
-
 <template>
-<customer-table :user-attributes="uiData.usersPageData.userAttributes" :users-details="usersData.userDetails.users"/>
+  <customer-table
+    :user-attributes="uiData.usersPageData.userAttributes"
+    :users-details="usersData.userDetails.users"
+  />
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<script setup lang="ts">
+import CustomerTable from "./components/CustomerTable.vue";
+import uiData from "./data/uiData.json";
+import usersData from "./data/usersData.json";
+</script>
