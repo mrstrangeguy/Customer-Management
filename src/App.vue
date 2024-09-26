a061ccc12b0b9004f5bf139ae3baab1eb91e4615
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-09-26 10:30:30 +0530

    refactor: remove unwanted files from app file

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,5 @@
 </template>
 
 <script setup lang="ts">
-import CustomerTable from "./components/CustomerTable.vue";
-import uiData from "./data/uiData.json";
-import usersData from "./data/usersData.json";
+
 </script>
