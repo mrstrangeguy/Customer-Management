0b1d2c760f977e93a569b7d7d67b7df50f84fbab
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-09-27 08:06:57 +0530

    refactor: change app file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import CustomerTable from './components/CustomerTable.vue';
-import uiData from './data/uiData.json'
-import usersData from './data/usersData.json'
-
-const userStatus = ref('commission')
+import ContactsPage from "./pages/ContactsPage.vue";
 </script>
 
 <template>
-<customer-table :user-attributes="uiData.usersPageData.userAttributes" :users-details="usersData.userDetails.users" :user-status="userStatus" />
+  <contacts-page />
 </template>
