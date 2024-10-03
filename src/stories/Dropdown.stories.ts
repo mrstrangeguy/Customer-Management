a3fdb80e98540fbf78c8ff9e21ee6a1e8ba8ed13
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-10-03 12:19:26 +0530

    refactor: updated story

--- src/stories/Dropdown.stories.ts
+++ src/stories/Dropdown.stories.ts
@@ -2,7 +2,6 @@ import type { Meta, StoryObj } from "@storybook/vue3";
 
 import Dropdown from "../components/Dropdown.vue";
 import uiData from "../data/uiData.json";
-import { DropdownPositions } from "../Constants";
 
 const { userStatuses } = uiData.usersPageData.filterDropDown;
 
@@ -14,17 +13,10 @@ export default meta;
 type Story = StoryObj<typeof Dropdown>;
 
 export const DropdownComponent: Story = {
-  argTypes: {
-    contentPosition: {
-      options: Object.values(DropdownPositions),
-      control: "select",
-    },
-  },
   args: {
     text: "ALL",
     headerStyle: "hover:bg-slate-200 py-1.5 pl-3 pr-2",
     dropDownItems: userStatuses,
-    contentPosition: DropdownPositions.Left,
   },
   render: (args) => ({
     components: { Dropdown },
