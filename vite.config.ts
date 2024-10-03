32a855cc25056f4b94448645cedededf9e28ecac
Vidhu Krishnan <vidhuk2003@gmail.com>
2024-10-03 09:19:27 +0530

    refactor: add base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vitest/config'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  test:{
-    globals:true,
-    environment:"jsdom"
+  base: "/Customer-Management/",
+  test: {
+    globals: true,
+    environment: "jsdom",
   },
   css: {
     preprocessorOptions: {
       css: {
-        additionalData: '@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");'
+        additionalData:
+          '@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");',
       },
     },
   },
-})
+});
